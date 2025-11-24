834f45261bdb248dcf977518be60af34220ca47c
Rod Weis <rodweis@gmail.com>
2025-11-23 18:36:25 -0700

    Add unit tests for text nodes

---
+++ src/test_textnode.py
@@ -0,0 +1,37 @@
+import unittest
+
+from textnode import TextNode, TextType
+
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertEqual(node, node2)
+
+    def test_not_eq_text_delta(self):
+        node = TextNode("This is a text node", TextType.ITALIC)
+        node2 = TextNode("This is a different text node", TextType.ITALIC)
+        self.assertNotEqual(node, node2)
+
+    def test_not_eq_style_delta(self):
+        node = TextNode("This is a text node", TextType.PLAIN)
+        node2 = TextNode("This is a text node", TextType.CODE)
+        self.assertNotEqual(node, node2)
+
+    def test_not_eq_url_delta(self):
+        node = TextNode("Click here", TextType.LINK, url="https://example.com")
+        node2 = TextNode("Click here", TextType.LINK, url="https://different.com")
+        self.assertNotEqual(node, node2)
+
+    def test_not_eq_url_presence_delta(self):
+        node = TextNode("Click here", TextType.LINK, url="https://example.com")
+        node2 = TextNode("Click here", TextType.LINK)
+        self.assertNotEqual(node, node2)
+
+    def test_not_eq_different_type(self):
+        node = TextNode("This is a text node", TextType.PLAIN)
+        self.assertNotEqual(node, "This is a text node")
+
+if __name__ == "__main__":
+    unittest.main()
